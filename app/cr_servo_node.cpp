6cba4d4730eb3f72f7a68beb308f90046307098e
Nico <n.chowchinsung@gmail.com>
2025-10-15 13:50:06 +0000

    [add] command writing for the servo node with interface updates.

--- app/cr_servo_node.cpp
+++ app/cr_servo_node.cpp
@@ -2,10 +2,12 @@
 #include "maestro_device.h"
 
 #include "cr_hand_interfaces/msg/hand_state.hpp"
+#include "cr_hand_interfaces/msg/hand_command.hpp"
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/joint_state.hpp"
 
+#include <algorithm>
 #include <chrono>
 #include <cstdio>
 #include <stdexcept>
@@ -28,10 +30,27 @@ public:
             RCLCPP_INFO(get_logger(), "Pololu Maestro device opened!");
         }
 
-        // initialize servo status
-        this->servoStatus = new MaestroProtocol::ServoStatus[this->maestroDevice->GetServoCount()];
+        // initialize servo data
+        this->servoStatus = new MaestroProtocol::ServoStatus[this->maestroDevice->GetServoCount()]();
+        this->servoCommand = new double[this->maestroDevice->GetServoCount()]();
+        std::fill(this->servoCommand, this->servoCommand + this->maestroDevice->GetServoCount(), 0.0);
+
+        // create a reentrant callback group
+        auto reentrant_group = this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
+        rclcpp::SubscriptionOptions sub_options;
+        sub_options.callback_group = reentrant_group;
 
         // hand command subscriber
+        this->sub_hand_command = this->create_subscription<cr_hand_interfaces::msg::HandCommand>("/hand_command", 10,
+            [this](const cr_hand_interfaces::msg::HandCommand::SharedPtr msg)
+            {
+                this->servoCommand[0] = msg->palm_pos_cmd;
+                this->servoCommand[1] = msg->thumb_pos_cmd;
+                this->servoCommand[2] = msg->index_pos_cmd;
+                this->servoCommand[3] = msg->middle_pos_cmd;
+                this->servoCommand[4] = msg->ring_pos_cmd;
+                this->servoCommand[5] = msg->little_pos_cmd;
+            }, sub_options);
 
         // hand state publisher
         this->pub_hand_state = this->create_publisher<cr_hand_interfaces::msg::HandState>("/hand_state", 10);
@@ -41,8 +60,8 @@ public:
             {
                 this->ReadState();
                 this->pubState();
-            }
-        );
+                this->WriteCommand();
+            });
     }
 
     ~MaestroNode()
@@ -55,40 +74,42 @@ public:
     }
 
 private:
-    rclcpp::Publisher<cr_hand_interfaces::msg::HandState>::SharedPtr pub_hand_state;
-    MaestroDevice *maestroDevice;
-    MaestroProtocol::ServoStatus *servoStatus;
     int time_pub_state_ = 20;
     rclcpp::TimerBase::SharedPtr exec_state_timer_;
+    rclcpp::Publisher<cr_hand_interfaces::msg::HandState>::SharedPtr pub_hand_state;
+    rclcpp::Subscription<cr_hand_interfaces::msg::HandCommand>::SharedPtr sub_hand_command;
+
+    MaestroDevice *maestroDevice;
+    MaestroProtocol::ServoStatus *servoStatus;
+    double *servoCommand;
 
     void pubState()
     {
         auto msg_HandState = cr_hand_interfaces::msg::HandState();
+        msg_HandState.palm = sensor_msgs::msg::JointState();
         msg_HandState.thumb = sensor_msgs::msg::JointState();
         msg_HandState.index = sensor_msgs::msg::JointState();
         msg_HandState.middle = sensor_msgs::msg::JointState();
         msg_HandState.ring = sensor_msgs::msg::JointState();
         msg_HandState.little = sensor_msgs::msg::JointState();
 
+        msg_HandState.palm.name.push_back("palm");
+        msg_HandState.palm.position.push_back(this->maestroDevice->ConvertToJointPosition(this->servoStatus[0].position));
+
         msg_HandState.thumb.name.push_back("thumb");
-        msg_HandState.thumb.position.push_back(this->maestroDevice->ConvertToJointPosition(this->servoStatus[0].position));
-        msg_HandState.thumb.velocity.push_back(this->servoStatus[0].speed);
+        msg_HandState.thumb.position.push_back(this->maestroDevice->ConvertToJointPosition(this->servoStatus[1].position));
 
         msg_HandState.index.name.push_back("index");
-        msg_HandState.index.position.push_back(this->maestroDevice->ConvertToJointPosition(this->servoStatus[1].position));
-        msg_HandState.index.velocity.push_back(this->servoStatus[1].speed);
+        msg_HandState.index.position.push_back(this->maestroDevice->ConvertToJointPosition(this->servoStatus[2].position));
 
         msg_HandState.middle.name.push_back("middle");
-        msg_HandState.middle.position.push_back(this->maestroDevice->ConvertToJointPosition(this->servoStatus[2].position));
-        msg_HandState.middle.velocity.push_back(this->servoStatus[2].speed);
+        msg_HandState.middle.position.push_back(this->maestroDevice->ConvertToJointPosition(this->servoStatus[3].position));
 
         msg_HandState.ring.name.push_back("ring");
-        msg_HandState.ring.position.push_back(this->maestroDevice->ConvertToJointPosition(this->servoStatus[3].position));
-        msg_HandState.ring.velocity.push_back(this->servoStatus[3].speed);
+        msg_HandState.ring.position.push_back(this->maestroDevice->ConvertToJointPosition(this->servoStatus[4].position));
 
         msg_HandState.little.name.push_back("little");
-        msg_HandState.little.position.push_back(this->maestroDevice->ConvertToJointPosition(this->servoStatus[4].position));
-        msg_HandState.little.velocity.push_back(this->servoStatus[4].speed);
+        msg_HandState.little.position.push_back(this->maestroDevice->ConvertToJointPosition(this->servoStatus[5].position));
 
         this->pub_hand_state->publish(msg_HandState);
     }
@@ -104,12 +125,19 @@ private:
                 this->servoStatus[i].target = tmp_servoStatus[i].target;
                 this->servoStatus[i].speed = tmp_servoStatus[i].speed;
                 this->servoStatus[i].acceleration = tmp_servoStatus[i].acceleration;
+                printf("[Maestro] get servo=%u position=%u target=%u speed=%u\n",
+                     i, tmp_servoStatus[i].position, tmp_servoStatus[i].target, tmp_servoStatus[i].speed);
             }
         }
     }
 
     void WriteCommand()
-    {}
+    {
+        for (auto i = 0; i < this->maestroDevice->GetServoCount(); i++)
+        {
+            this->maestroDevice->SetPosition(i, this->maestroDevice->ConvertToPWMPosition(this->servoCommand[i]));
+        }
+    }
 };
 
 int main(int argc, char *argv[])
